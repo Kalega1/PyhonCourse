b7d2e25a4ee721492014c4732f5e23e53a2e35b4
Kalega1 <kalegin.m@yandex.ru>
2023-04-10 14:00:53 +0500

    urok7

--- main.py
+++ main.py
@@ -21,16 +21,54 @@
 ########################
 
 
-def sum(a, b):
-    if b == 0:
-        return a
-    else:
-        return sum(a ^ b, (a & b) << 1)
+# def sum(a, b):
+#     if b == 0:
+#         return a
+#     else:
+#         return sum(a ^ b, (a & b) << 1)
 
-a = 2
-b = 2
-result = sum(a, b)
-print(result) # Output: 4
+# a = 2
+# b = 2
+# result = sum(a, b)
+# print(result) # Output: 4
+
+########################
+
+# import re
+
+# poem = input()  # Ввод стихотворения
+# words = poem.split()  # Разбиваем стихотворение на список слов
+
+# vowels = ['a', 'e', 'i', 'o', 'u', 'y']  # Список гласных букв (включая "y")
+# vowel_count = 0  # Количество гласных в предыдущей фразе
+
+# for word in words:
+#     # Ищем гласные буквы в слове и считаем их количество
+#     current_vowel_count = len(re.findall('[' + ''.join(vowels) + ']', word))
+
+#     if vowel_count == 0:
+#         vowel_count = current_vowel_count  # Если это первая фраза, запоминаем количество гласных
+#     elif current_vowel_count != vowel_count:
+#         print('Пам парам')  # Если количество гласных отличается от предыдущей фразы, ритма нет
+#         break
+# else:
+#     print('Парам пам-пам') 
+
+
+###############################
+
+
+# def print_operation_table(operation, num_rows=6, num_columns=6):
+#     for i in range(1, num_rows + 1):
+#         row = ""
+#         for j in range(1, num_columns + 1):
+#             row += str(operation(i, j)) + " "
+#         print(row.strip())
+
+# print_operation_table(lambda x, y: x * y)
+
+
+
 
 
 
